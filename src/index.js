f4c61bb868595f13688c552ba3817b3e6de618e2
SameAsNormal <16752058+SameAsNormal@users.noreply.github.com>
2021-04-30 21:28:12 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const {
+  Client
+} = require("discord.js");
+const client = new Client();
+const token = process.env.TOKEN;
+
+client.login(token);
+
+client.on("ready", () => {
+  console.log(`${client.user.username} | now online!`);
+
+  client.user.setActivity("the hit game Among Us", {
+    type: "PLAYING"
+  });
+});
+
+client.on("message", message => {
+  if (message.author.bot) return;
+
+  if (message.content.toLowerCase().includes("sus") || 
+    message.content.toLowerCase().includes("sussy") || 
+    message.content.toLowerCase().includes("amongus") || 
+    message.content.toLowerCase().includes("amugus") || 
+    message.content.toLowerCase().includes("amongus") || 
+    message.content.toLowerCase().includes("among us")
+  ) {
+    message.reply("HAHAH sus xD");
+  }
+})
