634ea09b24917be27385f17d404661e1d4f4d034
SameAsNormal <16752058+LunasAWolf@users.noreply.github.com>
2021-05-16 16:51:12 -0700

    much better code for random int

--- src/index.js
+++ src/index.js
@@ -16,7 +16,9 @@ const phrases = [
 ];
 
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
 client.login(token);
