2876bab7c749fda7ba132887698ee9110de58bc7
SameAsNormal <16752058+LunasAWolf@users.noreply.github.com>
2021-05-16 15:25:05 -0700

    randomly responds to some messages

--- src/index.js
+++ src/index.js
@@ -26,11 +26,24 @@ client.on("ready", () => {
   });
 });
 
+client.on("guildCreate", guild => {
+  console.log(`${client.user.username} | Added to guild ${guild.name} by ${guild.owner}`);
+});
+
+client.on("guildDelete", guild => {
+  console.log(`${client.user.username} | Removed from guild ${guild.name}`);
+});
+
 client.on("message", message => {
   if (message.author.bot || !message.guild) return;
 
   const args = message.content.toLowerCase().split(" ");
   for (var i in phrases) {
     if (args.includes(phrases[i].toLowerCase())) return message.reply("haha sus xD");
+  }
+
+  let num = Math.random(1, 100);
+  if (num == 50) {
+    return message.channel.send(`${message.author} is the sussy imposter`);
   }
 });
