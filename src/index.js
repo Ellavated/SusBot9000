3d9ea31579d8e5289ed36a7013e24fc17bd52972
SameAsNormal <16752058+LunasAWolf@users.noreply.github.com>
2021-05-31 20:01:18 -0700

    some stuff and things

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const {
 } = require("discord.js");
 const client = new Client();
 const token = process.env.TOKEN;
+const owner_id = "136631672425807872";
 
 const phrases = [
   "sus",
@@ -23,12 +24,12 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
+// BOT STARTS HERE
+
 client.login(token);
 
 client.on("ready", () => {
   console.log(`${client.user.username} | now online!`);
-  console.log(`I am in ${client.guilds.cache.size} servers`);
-
   client.user.setActivity("the hit game Among Us", {
     type: "PLAYING"
   });
@@ -44,54 +45,55 @@ client.on("guildDelete", guild => {
 
 client.on("message", message => {
   if (message.author.bot || !message.guild) return;
-  if (message.channel.type != "text") return;
+  if (!message.guild.me.hasPermission("SEND_MESSAGES") || !message.guild.me.hasPermission("VIEW_CHANNEL")) return; // this was an actual issue in servers for some reason
 
   const args = message.content.toLowerCase().split(" ");
-  // help command
-  if (args[0] == "-help") {
-    let embed = new MessageEmbed()
-      .setColor("RED")
-      .setTitle(`Help for ${client.user.username}`)
-      .setThumbnail(client.user.displayAvatarURL())
-      .setDescription("To use to bot simply type 'sus'! There is even a rare chance for a special message when you type :3")
-      .setFooter(`Created by LunaTheFloof#8447`)
-      .setTimestamp();
-    return message.channel.send(embed);
-  } else if (args[0] == "-count") { // count command
-    if (message.author.id != "136631672425807872") return;
-    let embed = new MessageEmbed()
-      .setColor("RED")
-      .setTitle("Server Count!")
-      .setThumbnail(client.user.displayAvatarURL())
-      .setDescription(`Counts the number of servers the bot is in`)
-      .addFields(
-        {
-          name: 'Total Servers',
-          value: client.guilds.cache.size,
-          inline: true
-        },
-        {
-          name: 'Total Channels',
-          value: client.channels.cache.size,
-          inline: true,
-        },
-        {
-          name: 'Total Users',
-          value: client.users.cache.size,
-          inline: true
-        }
-      )
-      .setFooter(`Created by LunaTheFloof#8447`)
-      .setTimestamp();
-    return message.channel.send(embed);
+  switch (args[0]) {
+    case "-help": // help command
+      let helpEmbed = new MessageEmbed()
+        .setColor("RED")
+        .setTitle(`Help for ${client.user.username}`)
+        .setThumbnail(client.user.displayAvatarURL())
+        .setDescription("To use to bot simply type 'sus'! There is even a rare chance for a special message when you type :3")
+        .setFooter(`Created by LunaTheFloof#8447`)
+        .setTimestamp();
+      return message.channel.send(helpEmbed);
+    case "-count": // count command
+      if (message.author.id != owner_id) return;
+      let countEmbed = new MessageEmbed()
+        .setColor("RED")
+        .setTitle("Server Count!")
+        .setThumbnail(client.user.displayAvatarURL())
+        .setDescription(`Counts the number of servers the bot is in`)
+        .addFields(
+          {
+            name: 'Total Servers',
+            value: client.guilds.cache.size,
+            inline: true
+          },
+          {
+            name: 'Total Channels',
+            value: client.channels.cache.size,
+            inline: true,
+          },
+          {
+            name: 'Total Users',
+            value: client.users.cache.size,
+            inline: true
+          }
+        )
+        .setFooter(`Created by LunaTheFloof#8447`)
+        .setTimestamp();
+      return message.channel.send(countEmbed);
   }
 
-
-  for (var i in phrases) {
+  for (let i in phrases) {
     if (args.includes(phrases[i].toLowerCase())) return message.reply("haha sus xD");
   }
-  let num = getRandomInt(1, 51);
-  if (num == 10) {
-    message.channel.send(`${message.author} is lookin kinda sussy 😳`);
+  let num = getRandomInt(1, 51); // returns any integer between 1 and 50.
+  switch (num) {
+    case 10:
+      message.channel.send(`${message.author} is looking kinda sussy 😳`);
+      return;
   }
 });
