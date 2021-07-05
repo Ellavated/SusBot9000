1235c6f04d7576e3d23f48b24f34c7b0c4335e5e
SameAsNormal <16752058+LunasAWolf@users.noreply.github.com>
2021-07-05 01:13:41 -0700

    test

--- src/index.js
+++ src/index.js
@@ -49,6 +49,8 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
 }
 
+//a
+
 // BOT STARTS HERE
 
 client.login(token);
