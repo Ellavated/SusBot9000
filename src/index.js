7b24d33df5b9dce8138e6b879018d5775e6445d0
SameAsNormal <16752058+LunasAWolf@users.noreply.github.com>
2021-09-28 21:27:10 -0700

    fixing the consitant error

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
-const { Client, Collection } = require('discord.js');
+const {
+  Client,
+  Collection
+} = require('discord.js');
 const fs = require('fs');
 
 const client = new Client({
-  intents: [ 'GUILDS', 'GUILD_MESSAGES' ],
+  intents: ['GUILDS', 'GUILD_MESSAGES'],
   allowedMentions: {
     parse: ['users', 'roles'],
     repliedUser: false
@@ -84,7 +87,9 @@ client.login(token);
 // vvv client events vvv
 client.on("ready", () => {
   console.log(`${client.user.username} | now online!`);
-  client.user.setActivity("the hit game Among Us", { type: "PLAYING" });
+  client.user.setActivity("the hit game Among Us", {
+    type: "PLAYING"
+  });
 });
 
 client.on("guildCreate", guild => {
@@ -99,9 +104,7 @@ client.on("messageCreate", async message => {
   // checks
   if (message.author.bot || !message.guild) return;
   if (!message.guild.me.permissions.has("SEND_MESSAGES") || !message.guild.me.permissions.has("VIEW_CHANNEL") || !message.guild.me.permissions.has("READ_MESSAGE_HISTORY")) return;
-
-  // prevents discordAPI error code 160002
-  if (message.channel.id == "702109440559546368") return;
+  if (!message.guild.me.permissionsIn(message.channel.id).has("READ_MESSAGE_HISTORY") || !message.guild.me.permissionsIn(message.channel.id).has("SEND_MESSAGES") || !message.guild.me.permissionsIn(message.channel.id).has("VIEW_CHANNEL")) return;
 
   if (message.content.startsWith(prefix)) {
     const args = message.content.slice(prefix.length).trim().split(/ + /g);
@@ -124,6 +127,9 @@ client.on("messageCreate", async message => {
       if (words.includes(phrases[i].toLowerCase())) return message.reply({
         content: `${replies[Math.floor(Math.random() * replies.length)]}`,
         repliedUser: true
+      }).catch(err => {
+        console.log(err);
+        return;
       });
     }
     // * L - need to decide what to do with these. May just delete them
